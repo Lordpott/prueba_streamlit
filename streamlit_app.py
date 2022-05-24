d442555459f8b3589ea43c13984b6902e0e2df6e
Cristhian Jimenez <67933045+Lordpott@users.noreply.github.com>
2022-05-24 00:09:03 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -84,11 +84,12 @@ labels = 'Frogs', 'Hogs', 'Dogs', 'Logs'
 sizes = [15, 30, 45, 10]
 explode = (0, 0.1, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
 
-newchart = plt.figure(figsize = (10, 5))
+
 fig1, ax1 = plt.subplots()
 ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
         shadow=True, startangle=90)
 ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+newchart = plt.figure(figsize = (10, 5))
 
-plt.show()
+#plt.show()
 st.pyplot(newchart)

8a1a52e09969eb0e576cc57d8a78d59cbd432fd0
Cristhian Jimenez <67933045+Lordpott@users.noreply.github.com>
2022-05-25 11:50:01 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -118,9 +118,9 @@ df_cities = df['name']
 df_capacity = df['percentile_cont']
 
 st.subheader('Guest capacity by city')
-guest_capacity_city = plt.figure(figsize = (10, 5))
+# guest_capacity_city = plt.figure(figsize = (10, 5))
 
-fig1, ax1 = plt.subplots()
+guest_capacity_city, ax1 = plt.subplots()
 ax1.pie(df_capacity, labels=df_cities, autopct='%1.1f%%', shadow=True, startangle=90)
 
 st.pyplot(guest_capacity_city)

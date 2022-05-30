e3ddf63c3ff7374a739979ef09aeeffb72d727b5
Cristhian Jimenez <67933045+Lordpott@users.noreply.github.com>
2022-05-30 11:07:22 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -138,7 +138,6 @@ df = pd.DataFrame({'nb_people':[8,3,4,2], 'group':["group A", "group B", "group 
 fig2 = plt.figure(figsize = (10, 5))
 squarify.plot(sizes=df['nb_people'], label=df['group'], alpha=.8 )
 plt.axis('off')
-plt.show()
 st.pyplot(fig2)
 
 

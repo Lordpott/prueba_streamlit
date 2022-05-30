678478c087b71396e812245968216b1f12c9dc9b
Cristhian Jimenez <67933045+Lordpott@users.noreply.github.com>
2022-05-30 09:56:16 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -100,8 +100,8 @@ data2 = np.sin(0.5 * np.pi * t)
 fig, ax1 = plt.subplots()
 
 color = 'tab:pink'
-ax1.set_xlabel('time (s)')
-ax1.set_ylabel('exp', color=color)
+ax1.set_xlabel('prices')
+ax1.set_ylabel('cities', color=color)
 ax1.plot(t, data1, color=color)
 ax1.tick_params(axis='y', labelcolor=color)
 

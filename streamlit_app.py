f2306e0744e584cc8d514d01c0afdad75cbebc6b
Cristhian Jimenez <67933045+Lordpott@users.noreply.github.com>
2022-05-30 10:15:40 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -98,14 +98,14 @@ fig, ax1 = plt.subplots()
 color = 'tab:red'
 ax1.set_xlabel('cities')
 ax1.set_ylabel('min prices', color=color)
-ax1.plot(df_min, color=color)
+ax1.plot(df_cities, df_min, color=color)
 ax1.tick_params(axis='y', labelcolor=color)
 
 ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
 
 color = 'tab:blue'
 ax2.set_ylabel('max prices', color=color)  # we already handled the x-label with ax1
-ax2.plot(df_max, color=color)
+ax2.plot(df_cities, df_max, color=color)
 ax2.tick_params(axis='y', labelcolor=color)
 
 fig.tight_layout()  # otherwise the right y-label is slightly clipped

fa5238af5de45ab51db8f6131eb88bbcd734943f
Cristhian Jimenez <67933045+Lordpott@users.noreply.github.com>
2022-05-30 12:11:53 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -166,7 +166,7 @@ st.subheader('Guest capacity by city')
 
 x = df_cities
 y = df_starts_avg
-s = df_starts_count
+s = df_starts_avg
 
 fig3 = plt.figure(figsize = (10, 5))
 plt.scatter(x, y, s, c="g", alpha=0.5, label="Stars")
